304f479d059f3e1110f8968e4bd62e72cb30a72c
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2025-08-04 14:55:49 +0530

    files of TYBCS

--- TYBCS/SEM-1/DataAnalyst/EvenNumbersList.py
+++
@@ -1,3 +0,0 @@
-#even numbers in an list
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-def EvenNumbersList(a):
--- TYBCS/SEM-1/DataAnalyst/SumOfArray.py
+++
@@ -1,9 +0,0 @@
-# sum of array 
-import numpy as np
-def SumOfArray():
-	a = np.array([1,2,3])
-	sum = 0
-	for i in a:
-		sum += i;
-	print(sum)
-SumOfArray()
